5ed0fae611f6b7c79beaae1051b7080f52179859
Paulo Vidal <u1d4lp@gmail.com>
2021-12-04 13:47:01 -0300

    Update Auction.sol

--- ethereum/courses/dumitrescu-ether/dumitrescu-projects/2-auction/Auction.sol
+++ ethereum/courses/dumitrescu-ether/dumitrescu-projects/2-auction/Auction.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 
 contract Auction {
     address payable public owner;
-    address payable highestBider;
+    address payable highestBidder;
     uint public startBlock;
     uint public endBlock;
     uint public bidIncrement;
@@ -27,5 +27,46 @@ contract Auction {
         bidIncrement = 100;
     }
 
-    function placeBid() {}
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    modifier notOwner() {
+        require(msg.sender != owner);
+        _;
+    }
+
+    modifier afterStart() {
+        require(block.number >= startBlock);
+        _;
+    }
+
+    modifier beforeEnd() {
+        require(block.number <= endBlock);
+        _;
+    }
+
+    function min(uint a, uint b) pure internal returns(uint) {
+        if(a <= b){
+            return a;
+        } else {
+            return b;
+        }
+
+    }
+
+    function placeBid() public payable notOwner afterStart beforeEnd {
+        require(auctionState == State.Running);
+        require(msg.value >= 100);
+        uint currentBid = bids[msg.sender] + msg.value;
+        require(currentBid > highestBindingBid);
+        bids[msg.sender] = currentBid;
+        if(currentBid <= bids[highestBidder]){
+            highestBindingBid = min(currentBid + bidIncrement, bids[highestBidder]);
+        } else {
+            highestBindingBid = min(currentBid, bids[highestBidder] + bidIncrement);
+            highestBidder = payable(msg.sender);
+        }
+    }
 }
