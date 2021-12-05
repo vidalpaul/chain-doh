d002381f2cd30f2b528162e848ca92272710ac1e
Paulo Vidal <u1d4lp@gmail.com>
2021-12-05 14:05:54 -0300

    implement Request struct and createRequest function

--- ethereum/courses/dumitrescu-ether/dumitrescu-projects/3-crowdfunding/Crowdfunding.sol
+++ ethereum/courses/dumitrescu-ether/dumitrescu-projects/3-crowdfunding/Crowdfunding.sol
@@ -2,14 +2,14 @@
 
 pragma solidity ^0.8.0;
 
-contract CampaignFactory{
-    Campaign[] public deployedCampaigns;
+// contract CampaignFactory{
+//     Campaign[] public deployedCampaigns;
 
-    function deployCampaign(uint goal, uint deadline) public {
-        Campaign newCampaign = new Campaign(msg.sender, goal, deadline);
-        deployedCampaigns.push(newCampaign);
-    }
-}
+//     function deployCampaign(uint goal, uint deadline) public {
+//         Campaign newCampaign = new Campaign(msg.sender, goal, deadline);
+//         deployedCampaigns.push(newCampaign);
+//     }
+// }
 
 contract Campaign {
     address payable admin;
@@ -18,16 +18,27 @@ contract Campaign {
     uint public goal;
     uint public deadline; // timestamp
     uint public raisedAmount;
+    uint public nOfRequests;
     mapping(address => uint) public contributors;
+    struct Request {
+        string description;
+        address payable recipient;
+        uint value;
+        bool completed;
+        uint nOfVoters;
+        mapping(address => bool) voters;
+    }
+    mapping(uint => Request) requests;
 
-    constructor(address eoa, uint _goal, uint _deadline){
-        admin = payable(eoa);
+    constructor(uint _goal, uint _deadline){
+        // admin = payable(eoa);
+        admin = payable(msg.sender);
         goal = _goal;
         deadline = block.timestamp + _deadline;
         minimumContribution = 100 wei;
     }
 
-    modifier onlyOwner() {
+    modifier onlyAdmin() {
         require(msg.sender == admin);
         _;
     }
@@ -46,5 +57,27 @@ contract Campaign {
         contribute();
     }
 
-    
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
+
+    function getRefund() public {
+        require(block.timestamp > deadline && raisedAmount < goal);
+        require(contributors[msg.sender] > 0);
+        address payable recipient = payable(msg.sender);
+        uint value = contributors[msg.sender];
+        recipient.transfer(value);
+        contributors[msg.sender] = 0;
+    }
+
+    function createRequest(string memory _description, address payable _recipient, uint _value) public onlyAdmin {
+        Request storage newRequest = requests[nOfRequests];
+        newRequest.description = _description;
+        newRequest.recipient = _recipient;
+        newRequest.value = _value;
+        newRequest.completed = false;
+        newRequest.nOfVoters = 0;
+        nOfRequests++;
+    }
+
 }
