c1f2b350572f482b157c06bd45175d03176d3b20
Paulo Vidal <u1d4lp@gmail.com>
2021-12-04 21:54:16 -0300

    Update Crowdfunding.sol

--- ethereum/courses/dumitrescu-ether/dumitrescu-projects/3-crowdfunding/Crowdfunding.sol
+++ ethereum/courses/dumitrescu-ether/dumitrescu-projects/3-crowdfunding/Crowdfunding.sol
@@ -5,15 +5,30 @@ pragma solidity ^0.8.0;
 contract CampaignFactory{
     Campaign[] public deployedCampaigns;
 
-    function deployCampaign() public {
-        Campaign newCampaign = new Campaign(msg.sender);
+    function deployCampaign(uint goal, uint deadline) public {
+        Campaign newCampaign = new Campaign(msg.sender, goal, deadline);
         deployedCampaigns.push(newCampaign);
     }
 }
 
 contract Campaign {
-    address payable owner;
-    constructor(address eoa){
-        owner = payable(eoa);
+    address payable admin;
+    uint public nOfContributors;
+    uint public minimumContribution;
+    uint public goal;
+    uint public deadline; // timestamp
+    uint public raisedAmount;
+    mapping(address => uint) public contributors;
+
+    constructor(address eoa, uint _goal, uint _deadline){
+        admin = payable(eoa);
+        goal = _goal;
+        deadline = block.timestamp + _deadline;
+        minimumContribution = 100 wei;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == admin);
+        _;
     }
 }
