5fba016316f14701901610f9c36308f05bc1f19d
Paulo Vidal <u1d4lp@gmail.com>
2021-12-06 19:17:41 -0300

    implement voting function

--- ethereum/courses/dumitrescu-ether/dumitrescu-projects/3-crowdfunding/Crowdfunding.sol
+++ ethereum/courses/dumitrescu-ether/dumitrescu-projects/3-crowdfunding/Crowdfunding.sol
@@ -80,4 +80,12 @@ contract Campaign {
         nOfRequests++;
     }
 
+    function voteForRequest(uint _requestNo) public {
+        require(contributors[msg.sender] > 0, "You must be a contributor to vote");
+        Request storage thisRequest = requests[_requestNo];
+        require(thisRequest.voters[msg.sender] == false, "You have already voted");
+        thisRequest.voters[msg.sender] = true;
+        thisRequest.nOfVoters++;
+    }
+
 }
